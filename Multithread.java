45381715d5235932265f87480df7deb4987a9a24
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-11-04 18:36:46 -0800

    Create Multithread.java

---
+++ Multithread.java
@@ -0,0 +1,21 @@
+// extending the thread class 
+import java.util.*;  
+class MultithreadingDemo extends Thread{
+	public void run(){
+		try{
+			System.out.println("Thread " + Thread.currentThread().getId() + " is running");  
+		}
+		catch (Exception e){
+			System.out.println("Exception is caught");  
+		}	
+	}
+}
+public class Multithread{
+	public static void main(String[] args){
+		int n = 8; // number of threads 
+		for (int i = 0; i < n; i++){
+			MultithreadingDemo object = new MultithreadingDemo();  
+			object.start(); 
+		}	
+	}
+}
